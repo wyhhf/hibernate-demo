e30949d156866352409108e0b01c9a7b5a66c48f
wyhhf <100178826+wyhhf@users.noreply.github.com>
2022-07-31 12:03:48 +0800

    pull test commit

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hot-fix test");
         System.out.println("master test");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
