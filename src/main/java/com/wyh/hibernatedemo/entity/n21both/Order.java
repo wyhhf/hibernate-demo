5bc8b6a1e75e6f5e01a57385e766d54660d1278a
王怡贺 <850005092@qq.com>
2022-08-07 14:59:54 +0800

    hibernate学习day03 commit

---
+++ src/main/java/com/wyh/hibernatedemo/entity/n21both/Order.java
@@ -0,0 +1,31 @@
+package com.wyh.hibernatedemo.entity.n21both;
+
+public class Order {
+    private Integer orderId;
+    private String orderName;
+    private Customer customer;
+
+    public Integer getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Integer orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getOrderName() {
+        return orderName;
+    }
+
+    public void setOrderName(String orderName) {
+        this.orderName = orderName;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+}
